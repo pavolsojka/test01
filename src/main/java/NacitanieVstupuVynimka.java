c2e57f749212f14a5151af9408b0ed528fa14f7c
pavolsojka <pavol.sojka@euba.sk>
2019-02-20 17:31:10 +0100

    Initial commit

---
+++ src/main/java/NacitanieVstupuVynimka.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NacitanieVstupuVynimka {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("zadaj meno a priezvisko");
+        String meno = sc.nextLine();
+        System.out.println("zadaj vek");
+        int vek = sc.nextInt();
+        if(vek < 0) {
+
+            System.out.println("Zadali ste zaporny vek");
+            System.exit(1);
+        }
+        sc.close();
+
+        System.out.println("meno: " + meno);
+        System.out.println("vek: " + vek);
+
+    }
+
+}
